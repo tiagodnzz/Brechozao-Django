ca9938c15799395c8b2fb781023b496bfc227ee0
devdantas <mauriciodantas.contato01@bol.com.br>
2019-11-21 20:11:37 -0300

    Primeiro commit bem avançado

---
+++ website/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    nome = 'Groger'
+    idade = 18
+    lista_roupas = [
+        'Boné da lacoste',
+        'Boné da Cycl0ne',
+        'Cinto do Bátima',
+        'Toca da Medusa',
+        'Óculos Rift Zika'
+    ]
+
+    context = {
+        'roupas': lista_roupas,
+        'nome': nome,
+        'idade': idade
+    }
+    
+    return render(request, 'home.html', context)
